388451e23ce106a230a123d196439ea5c64a3ff2
unknown <abdenour.didi@student.vinci.be>
2022-02-21 18:57:59 +0100

    feat: methode rechercheParPseudo

--- src/main/java/be/vinci/pae/donnees/DAO/UtilisateurDAO.java
+++ src/main/java/be/vinci/pae/donnees/DAO/UtilisateurDAO.java
@@ -11,9 +11,20 @@ public class UtilisateurDAO {
     private ServicesDAL servicesDAL;
 
     public UtilisateurDTO rechercheParPseudo(String pseudo){
-        servicesDAL.getPs("SELECT u.id_utilisateur, u.pseudo, u.nom, u.prenom, u.mdp, u.gsm, u.est_admin FROM projet.utilisateurs u WHERE u.pseudo = pseudo");
-        UtilisateurDTO utilisateurDTO = factory.getUtilisateur();
-        utilisateurDTO.setIdUtilisateur();
+        try {
+            ResultSet rs = servicesDAL.getPs("SELECT u.id_utilisateur, u.pseudo, u.nom, u.prenom, u.mdp, u.gsm, u.est_admin FROM projet.utilisateurs u WHERE u.pseudo = pseudo").executeQuery();
+            UtilisateurDTO utilisateurDTO = factory.getUtilisateur();
+            utilisateurDTO.setIdUtilisateur(rs.getInt(1));
+            utilisateurDTO.setPseudo(rs.getString(2));
+            utilisateurDTO.setNom(rs.getString(3));
+            utilisateurDTO.setPrenom(rs.getString(4));
+            utilisateurDTO.setMdp(rs.getString(5));
+            utilisateurDTO.setGsm(rs.getString(6));
+            utilisateurDTO.setEstAdmin(rs.getBoolean(7));
+            return utilisateurDTO;
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     }
