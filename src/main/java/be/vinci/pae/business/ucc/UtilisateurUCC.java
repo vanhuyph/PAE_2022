4fb12ae509e86e9ad7dd8f0365c4cc3205d2c7e2
Van-Huy Pham <vanhuy.pham@student.vinci.be>
2022-02-22 19:11:31 +0100

    doc: méthode connexion UtilisateurUCC

--- src/main/java/be/vinci/pae/business/ucc/UtilisateurUCC.java
+++ src/main/java/be/vinci/pae/business/ucc/UtilisateurUCC.java
@@ -10,6 +10,12 @@ public class UtilisateurUCC {
   @Inject
   UtilisateurDAO utilisateurDAO;
 
+  /*
+   * Vérifie si le mot de passe de l'utilisateur est correct à sa connexion
+   * @param pseudo : le pseudo de l'utilisateur
+   * @param mdp : le mot de passe de l'utilisateur
+   * @exception : IllegalStateException est lancée si le mot de passe est incorrect
+   */
   public UtilisateurDTO connexion(String pseudo, String mdp) {
 
     Utilisateur utilisateur = (Utilisateur) utilisateurDAO.rechercheParPseudo(pseudo);
@@ -20,5 +26,5 @@ public class UtilisateurUCC {
       throw new IllegalStateException("Non autorisé");
     }
   }
-  
+
 }
