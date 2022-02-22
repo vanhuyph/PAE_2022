5683eb3f9095e017075594f567f431d1fb35070b
Leandro Silva de Carvalho <leandro.silvadecarval@student.vinci.be>
2022-02-22 12:24:53 +0100

    refactor: optimisation rechercheParPseudo et getPs

--- src/main/java/be/vinci/pae/donnees/DAO/UtilisateurDAO.java
+++ src/main/java/be/vinci/pae/donnees/DAO/UtilisateurDAO.java
@@ -3,17 +3,20 @@ package be.vinci.pae.donnees.DAO;
 import be.vinci.pae.business.domaine.DomaineFactory;
 import be.vinci.pae.business.domaine.UtilisateurDTO;
 import be.vinci.pae.donnees.DAO.services.ServicesDAL;
-
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UtilisateurDAO {
+
     private DomaineFactory factory;
     private ServicesDAL servicesDAL;
 
-    public UtilisateurDTO rechercheParPseudo(String pseudo){
+    public UtilisateurDTO rechercheParPseudo(String pseudo) {
+        UtilisateurDTO utilisateurDTO = factory.getUtilisateur();
         try {
-            ResultSet rs = servicesDAL.getPs("SELECT u.id_utilisateur, u.pseudo, u.nom, u.prenom, u.mdp, u.gsm, u.est_admin FROM projet.utilisateurs u WHERE u.pseudo = pseudo").executeQuery();
-            UtilisateurDTO utilisateurDTO = factory.getUtilisateur();
+            ResultSet rs = servicesDAL.getPs(
+                    "SELECT u.id_utilisateur, u.pseudo, u.nom, u.prenom, u.mdp, u.gsm, u.est_admin FROM projet.utilisateurs u WHERE u.pseudo = pseudo")
+                .executeQuery();
             utilisateurDTO.setIdUtilisateur(rs.getInt(1));
             utilisateurDTO.setPseudo(rs.getString(2));
             utilisateurDTO.setNom(rs.getString(3));
@@ -21,13 +24,14 @@ public class UtilisateurDAO {
             utilisateurDTO.setMdp(rs.getString(5));
             utilisateurDTO.setGsm(rs.getString(6));
             utilisateurDTO.setEstAdmin(rs.getBoolean(7));
-            return utilisateurDTO;
-        } catch (SQLException e){
+
+        } catch (SQLException e) {
             e.printStackTrace();
         }
+        return utilisateurDTO;
     }
 
-    }
+}
 
 
 
--- src/main/java/be/vinci/pae/donnees/DAO/services/ServicesDAL.java
+++ src/main/java/be/vinci/pae/donnees/DAO/services/ServicesDAL.java
@@ -7,27 +7,30 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class ServicesDAL {
-    private String url = null;
-    private Connection conn = null;
-    private PreparedStatement ps;
-    public ServicesDAL(){
-        url = "jdbc:postgresql://coursinfo.vinci.be:5432/dbabdenour_didi";
-        try{
-            conn = DriverManager.getConnection(url, "abdenour_didi", "batbat123");
-        } catch (SQLException e){
-            e.printStackTrace();
-            System.out.println("impossible de joindre le server!");
-            System.exit(1);
-        }
 
+  private String url = null;
+  private Connection conn = null;
+
+  public ServicesDAL() {
+    url = "jdbc:postgresql://coursinfo.vinci.be:5432/dbabdenour_didi";
+    try {
+      conn = DriverManager.getConnection(url, "abdenour_didi", "batbat123");
+    } catch (SQLException e) {
+      e.printStackTrace();
+      System.out.println("impossible de joindre le server!");
+      System.exit(1);
     }
 
-    public PreparedStatement getPs(String query){
-        try {
-            return conn.prepareStatement(query);
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
+  }
+
+  public PreparedStatement getPs(String query) {
+    PreparedStatement preparedStatement = null;
+    try {
+      preparedStatement = conn.prepareStatement(query);
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
+    return preparedStatement;
+  }
 
 }
