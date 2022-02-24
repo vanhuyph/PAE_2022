58804f4349d8e88ccef9fd621f4a44a97ba756f2
unknown <bastien.verstappen@student.vinci.be>
2022-02-24 15:30:12 +0100

    refactor:Config pour checkstyle

--- src/main/java/be/vinci/pae/utils/Config.java
+++ src/main/java/be/vinci/pae/utils/Config.java
@@ -1,27 +1,35 @@
 package be.vinci.pae.utils;
 
 
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 
 public class Config {
 
+
     private static Properties props;
 
+    /**
+     * charge les propriétés contenu dans file
+     *
+     * @param file fichier .properties
+     */
     public static void load(String file) {
+
         props = new Properties();
         try (InputStream input = new FileInputStream(file)) {
             props.load(input);
         } catch (IOException e) {
             throw new WebApplicationException(
-                    Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).type("text/plain").build());
+                Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage())
+                    .type("text/plain")
+                    .build());
         }
     }
 
