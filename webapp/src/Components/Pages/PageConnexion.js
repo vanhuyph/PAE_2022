9ba500f03d59329c3bd768951e9e6dfaaef7be4a
Van-Huy Pham <vanhuy.pham@student.vinci.be>
2022-02-28 17:25:03 +0100

    refactor: barre de navigation + typo

--- webapp/src/Components/Pages/PageConnexion.js
+++ webapp/src/Components/Pages/PageConnexion.js
@@ -1,6 +1,10 @@
-import {recupUtilisateurDonneesSession, creationDonneeSessionUtilisateur} from "../../utils/session"
+import {
+  creationDonneeSessionUtilisateur,
+  recupUtilisateurDonneesSession
+} from "../../utils/session"
 import {Redirect} from "../Router/Router";
 import Navbar from "../Navbar/Navbar";
+
 let pageCon = `
     <div class="page-connexion">
         <h2>Connexion</h2>
@@ -12,7 +16,6 @@ let pageCon = `
             <label for="mdp">Mot de passe</label>
             <div class="mdp-conteneur">
                 <input type="password" id="mdp" class="mdp">
-                
             </div>
             <div class="se-souvenir"><input type="checkbox" id="souvenir" name="souvenir"><label for="souvenir">Se souvenir de moi</label></div>
             <div id="messageErreur"></div>
@@ -31,14 +34,12 @@ const PageConnexion = () => {
 
   const utilisateur = recupUtilisateurDonneesSession()
 
-
-  if(utilisateur){
+  if (utilisateur) {
     Navbar()
     Redirect("/")
-  }else{
+  } else {
     formCon.addEventListener("submit", surConnexion)
   }
-
 }
 
 const surConnexion = (e) => {
@@ -50,21 +51,21 @@ const surConnexion = (e) => {
   }
   let souvenir = document.querySelector("#souvenir").checked
   fetch("/api/utilisateurs/connexion", {
-    method:"POST",
-    body:JSON.stringify(utilisateur),
-    headers:{
+    method: "POST",
+    body: JSON.stringify(utilisateur),
+    headers: {
       "Content-Type": "application/json",
     }
   })
   .then((reponse) => {
-    if(!reponse.ok){
-      throw new Error("Error code : " + reponse.status + " : " + reponse.statusText)
+    if (!reponse.ok) {
+      throw new Error(
+          "Error code : " + reponse.status + " : " + reponse.statusText)
     }
     return reponse.json();
   })
   .then((donnee) => surConUtilisateur(donnee, souvenir))
   .catch(err => surErreur(err))
-
 }
 
 const surConUtilisateur = (donnee, souvenir) => {
@@ -78,8 +79,13 @@ const surErreur = (err) => {
   let messageErreur = document.querySelector("#messageErreur");
   let erreurMessage = "";
   console.log(err)
-  if (err.message.includes("401")) erreurMessage = "Pseudo/mot de passe incorrect.";
-  else erreurMessage = err.message;
+  if (err.message.includes(
+      "401")) {
+    erreurMessage = "Pseudo ou mot de passe incorrect.";
+  } else {
+    erreurMessage = err.message;
+  }
   messageErreur.innerText = erreurMessage;
 }
+
 export default PageConnexion
