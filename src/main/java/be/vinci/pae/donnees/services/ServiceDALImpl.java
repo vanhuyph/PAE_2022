7ec10b4ad6cfb1026a9148f0700ee3535bd29e28
Leandro Silva de Carvalho <leandro.silvadecarval@student.vinci.be>
2022-03-15 17:48:14 +0100

    feat: transaction commettre et retourEnArrire

--- src/main/java/be/vinci/pae/donnees/services/ServiceDALImpl.java
+++ src/main/java/be/vinci/pae/donnees/services/ServiceDALImpl.java
@@ -35,7 +35,7 @@ public class ServiceDALImpl implements ServiceDAL, ServiceBackendDAL {
    *
    * @param requete : instruction SQL sous format String
    * @return : une instruction SQL precompile
-   * @throws SQLException : est lancée si l'instruction SQL n'a pas su se precompile
+   * @throws FatalException : est lancée si l'instruction SQL n'a pas su se precompile
    */
   @Override
   public PreparedStatement getPs(String requete) {
@@ -44,6 +44,7 @@ public class ServiceDALImpl implements ServiceDAL, ServiceBackendDAL {
       preparedStatement = conn.prepareStatement(requete);
     } catch (SQLException e) {
       e.printStackTrace();
+      throw new FatalException("Erreur de ps", e);
     }
     return preparedStatement;
   }
@@ -67,13 +68,47 @@ public class ServiceDALImpl implements ServiceDAL, ServiceBackendDAL {
     }
   }
 
+  /**
+   * Commet une transaction SQL.
+   *
+   * @throws FatalException : est lancée s'il y a déjà une transaction active ou un problème lors de
+   *                        la transaction
+   */
   @Override
   public void commettreTransaction() {
-
+    Connection c = threadConnexion.get();
+    if (c == null) {
+      throw new FatalException("Pas de connexion: la transaction n'a pas été commencé");
+    }
+    try {
+      c.commit();
+      conn.setAutoCommit(true);
+      threadConnexion.remove();
+      c.close();
+    } catch (SQLException e) {
+      throw new FatalException("Erreur de commit", e);
+    }
   }
 
+  /**
+   * Retour en arriève  d'une transaction SQL.
+   *
+   * @throws FatalException : est lancée s'il y a déjà une transaction active ou un problème lors de
+   *                        la transaction
+   */
   @Override
   public void retourEnArriereTransaction() {
-
+    Connection c = threadConnexion.get();
+    if (c == null) {
+      throw new FatalException("Pas de connexion: la transaction n'a pas été commencé");
+    }
+    try {
+      c.rollback();
+      conn.setAutoCommit(true);
+      threadConnexion.remove();
+      c.close();
+    } catch (SQLException e) {
+      throw new FatalException("Erreur de retour en arriere", e);
+    }
   }
 }
